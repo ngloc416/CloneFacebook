5661b881f18751c54aa54e983a8f2e08a1406501
hoanglong41012 <vuhoanglong72@gmail.com>
2023-02-10 20:50:30 +0700

    fix: rebase

---
+++ services/axiosClient.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const axiosClient = axios.create({
+  baseURL: 'http://192.168.1.193:5000/it4788',
+  headers: {
+  'content-type': 'application/json',
+  },
+  responseType: 'json',
+  // paramsSerializer: params => { return queryString.stringify(params)},
+});
+
+axiosClient.interceptors.request.use(async (config) => {
+  const currentToken = AsyncStorage.token;
+  if (currentToken) {
+    config.query.token = currentToken;
+  }
+  return config;
+})
+axiosClient.interceptors.response.use((response) => {
+  if (response && response.data) {
+    return response.data;
+  }
+  return response;
+  }, (error) => {
+    return error.response.data;
+});
+export default axiosClient;
